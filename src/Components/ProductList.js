31a0b3ff0691842b05c93137ab9d95a2cbcb8205
Md Shoaib Raza <razashoaib270@gmail.com>
2023-02-03 13:48:41 +0530

    added upVote Functionality

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -6,7 +6,7 @@ function ProductList(props) {
 // this keyword can simply be understood as a reference to a particular component and it only refers inside render() or return() function or some functions provided by React API 
 // to use this keyord outside render or return we have to bind this through a constructor() function
 // binding can be done like this
-
+// binding is required in class based components function based component don't need binding
 // constructor(props){
 //   super(props);
 //   this.someFunction = this.someFunction.bind(this);
@@ -22,12 +22,12 @@ function ProductList(props) {
         </div> 
         <div className="data">
             <p className='heading'> 
-              <i class='fas fa-angle-up' style={{fontSize:'25px'}} onClick = {handleVotes} /> &nbsp;&nbsp;
+              <i className='fas fa-angle-up' style={{fontSize:'25px'}} onClick = {handleVotes} /> &nbsp;&nbsp;
               {props.vote}  <br />
-              {props.title}
+              {props.title} &nbsp; {props.id}
             </p>
             <p className='describe'>
-              {props.description} &nbsp; {props.id}
+              {props.description} &nbsp; {props.prodKey}
             </p>
             <div className ="subavtr">
               <p className ='submited'>Submitted By: </p>
